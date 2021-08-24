70c20a7c409cc49c546df47ea82495c98d094f95
halah90 <halahsami901@gmail.com>
2021-08-25 02:06:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// const emp:any={
+//     name:'Halah',
+//     age:30
+// }
+// console.log(emp);
+// emp.name="Ahmad";
+// console.log(emp);
+// emp.address='Irbid';
+// var rect = { x: 0, y: 10, width: 15, height: 20 };
+// // Destructuring assignment
+// var {x, y, width, height} = rect;
+// console.log(x, y, width, height); // 0,10,15,20
+// rect.x = 10;
+// ({x, y, width, height} = rect); // assign to existing variables using outer parentheses
+// console.log(x, y, width, height); // 10,10,15,20
+var sport = ['Basket ball',
+    'Tennis', 'Football', 'soccer'];
+var top1 = sport[0], top2 = sport[1], otherSports = sport.slice(2);
+console.log(top1, top2);
+console.log(otherSports);
